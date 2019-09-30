058a658a3760d20bce2b5c608b3f88c4b0b0b585
Artur Daylidonis <artur@daylidonis.com>
2019-09-30 19:31:28 +1000

    Display random numbers on one die

--- js/script.js
+++ js/script.js
@@ -1,15 +1,140 @@
 // JavaScript Document
-
-// Insert Main Menu
-var mainMenu = document.getElementById("mainMenu");
-mainMenu.insertAdjacentHTML('afterbegin', '<ul><li><a href="https://apy.app/micro/">ApyMicro</a></li><li><a href="https://apy.app/mini/">ApyMini</a></li><li><a href="https://apy.app/about/">About</a></li><li> <a href="https://apy.app/contact/">Contacts</a></li></ul>');
+function generateRandomNumber(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
 
 function generateScore(player) {
 	let randomNumber = generateRandomNumber(6) + 1;
-	
-	alert("Player " + player + " number is " + randomNumber);
+//	alert("Player " + player + " number is " + randomNumber);
+	updateDie(randomNumber);
 }
 
-function generateRandomNumber(max) {
-	return Math.floor(Math.random() * Math.floor(max));
+function updateDie(number) {
+	// Identify the die
+	alert(number);
+//	if (number == 1) {
+//		alert("You're a lucky winner!");
+//	}
+	
+	let dots = document.querySelectorAll("#die1 .dot");
+	let frames = document.querySelectorAll("#die1 .dot-frame");
+	
+	if (number == 1) {
+		dots[0].style.visibility = "hidden";
+		frames[0].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[1].style.visibility = "hidden";
+		frames[1].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[2].style.visibility = "visible";
+		frames[2].style = "display: table; height: 33.33%; width: 100%; background-color: none; float: left;";
+		
+		dots[3].style.visibility = "hidden";
+		frames[3].style = "display: none; height: 33.33%; width: 0%; background-color: none; float: left;";
+		
+		dots[4].style.visibility = "hidden";
+		frames[4].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[5].style.visibility = "hidden";
+		frames[5].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+	} else if (number == 2) {
+		dots[0].style.visibility = "hidden";
+		frames[0].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[1].style.visibility = "visible";
+		frames[1].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[2].style.visibility = "hidden";
+		frames[2].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[3].style.visibility = "hidden";
+		frames[3].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[4].style.visibility = "visible";
+		frames[4].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[5].style.visibility = "hidden";
+		frames[5].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+	} else if (number == 3) {
+		dots[0].style.visibility = "hidden";
+		frames[0].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[1].style.visibility = "visible";
+		frames[1].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[2].style.visibility = "visible";
+		frames[2].style = "display: table; height: 33.33%; width: 100%; background-color: none; float: left;";
+		
+		dots[3].style.visibility = "hidden";
+		frames[3].style = "display: none; height: 33.33%; width: 0%; background-color: none; float: left;";
+		
+		dots[4].style.visibility = "visible";
+		frames[4].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[5].style.visibility = "hidden";
+		frames[5].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+	} else if (number == 4) {
+		dots[0].style.visibility = "visible";
+		frames[0].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[1].style.visibility = "visible";
+		frames[1].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[2].style.visibility = "hidden";
+		frames[2].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[3].style.visibility = "hidden";
+		frames[3].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[4].style.visibility = "visible";
+		frames[4].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[5].style.visibility = "visible";
+		frames[5].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+	} else if (number == 5) {
+		dots[0].style.visibility = "visible";
+		frames[0].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[1].style.visibility = "visible";
+		frames[1].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[2].style.visibility = "visible";
+		frames[2].style = "display: table; height: 33.33%; width: 100%; background-color: none; float: left;";
+		
+		dots[3].style.visibility = "hidden";
+		frames[3].style = "display: none; height: 33.33%; width: 100%; background-color: none; float: left;";
+		
+		dots[4].style.visibility = "visible";
+		frames[4].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[5].style.visibility = "visible";
+		frames[5].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+	} else if (number == 6) {
+		// Show all dots
+		dots[0].style.visibility = "visible";
+		frames[0].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[1].style.visibility = "visible";
+		frames[1].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[2].style.visibility = "visible";
+		frames[2].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[3].style.visibility = "visible";
+		frames[3].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[4].style.visibility = "visible";
+		frames[4].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+		
+		dots[5].style.visibility = "visible";
+		frames[5].style = "display: table; height: 33.33%; width: 50%; background-color: none; float: left;";
+	}
+	
+	/*
+	// Hide a dot
+	dotcells[4].querySelector(".dot").style = "display:none;";
+	// Display only one dot in the middle row
+			dotCells[2].style = "width:100%;"
+			dotCells[3].style = "display: none;"
+	*/
 }
