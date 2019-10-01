7291dab812bb803be8d12fce0db186dcd5b78b59
Artur Daylidonis <artur@daylidonis.com>
2019-10-01 14:45:11 +1000

    Update DrumKit game

--- drumkit/index.js
+++ drumkit/index.js
@@ -3,7 +3,35 @@
 
 // Handle keyboard entry
 document.addEventListener('keydown', function(event) {
-	playAudioWithCode(event.keyCode);
+	var buttonClass = "";
+	
+	switch(event.keyCode) {
+		case 87:
+			buttonClass = "w";
+			break;
+		case 65:
+			buttonClass = "a";
+			break;
+		case 83:
+			buttonClass = "s";
+			break;
+		case 68:
+			buttonClass = "d";
+			break;
+		case 74:
+			buttonClass = "j";
+			break;
+		case 75:
+			buttonClass = "k";
+			break;
+		case 76:
+			buttonClass = "l";
+			break;
+		default:
+			//...
+	}
+	
+	handleClick(event.keyCode, buttonClass);
 });
 
 // Handle clicks
@@ -13,65 +41,86 @@ buttons.forEach(function(element) {
 	let elementClass = element.classList[0];
 	var soundCode = 0;
 	
-	if (elementClass == "w") {
-		soundCode = 87;
-		element.style = "background-image: url('images/crash.png');"
-	} 
-	else if (elementClass == "a") {
-		soundCode = 65;
-		element.style = "background-image: url('images/kick.png');"
-	} 
-	else if (elementClass == "s") {
-		soundCode = 83;
-		element.style = "background-image: url('images/snare.png');"
-	} 
-	else if (elementClass == "d") {
-		soundCode = 68;
-		element.style = "background-image: url('images/tom1.png');"
-	} 
-	else if (elementClass == "j") {
-		soundCode = 74;
-		element.style = "background-image: url('images/tom2.png');"
-	} 
-	else if (elementClass == "k") {
-		soundCode = 75;
-		element.style = "background-image: url('images/tom3.png');"
-	} 
-	else if (elementClass == "l") {
-		soundCode = 76;
-		element.style = "background-image: url('images/tom4.png');"
-	} 
+	switch (elementClass) {
+		case "w":
+			soundCode = 87;
+			element.style = "background-image: url('images/crash.png');";
+			break;
+		case "a":
+			soundCode = 65;
+			element.style = "background-image: url('images/kick.png');";
+			break;
+		case "s":
+			soundCode = 83;
+			element.style = "background-image: url('images/snare.png');";
+			break;
+		case "d":
+			soundCode = 68;
+			element.style = "background-image: url('images/tom1.png');";
+			break;
+		case "j":
+			soundCode = 74;
+			element.style = "background-image: url('images/tom2.png');";
+			break;
+		case "k":
+			soundCode = 75;
+			element.style = "background-image: url('images/tom3.png');";
+			break;
+		case "l":
+			soundCode = 76;
+			element.style = "background-image: url('images/tom4.png');";
+			break;
+		default:
+			// ...
+	}
 	
 	// This is how you assign a function with an input as an event listener in JS :(
-	element.addEventListener("click", function(){ handleClick(soundCode); }); 
+	element.addEventListener("click", function(){ handleClick(soundCode, elementClass); }); 
 });
 
-function handleClick(soundCode) {
+function handleClick(soundCode, elementClass) {
 	playAudioWithCode(soundCode);
+	flashButton(elementClass);
 }
 
 function playAudioWithCode(code) {	
-	var audio = new Audio('sounds/crash.mp3');
-	if(code == 87) {
-		audio = new Audio('sounds/crash.mp3'); // w
-    }
-    else if(code == 65) {
-        audio = new Audio('sounds/kick-bass.mp3'); // a
-    }
-	else if(code == 83) {
-		audio = new Audio('sounds/snare.mp3'); // s
-    }
-	else if(code == 68) {
-        audio = new Audio('sounds/tom-1.mp3'); // d
-    }
-	else if(code == 74) {
-        audio = new Audio('sounds/tom-2.mp3'); // j
-    }
-	else if(code == 75) {
-        audio = new Audio('sounds/tom-3.mp3'); // k
-    }
-	else if(code == 76) {
-		audio = new Audio('sounds/tom-4.mp3'); // l
-    }
+	var audio = new Audio('');
+	
+	switch(code) {
+		case 87:
+			audio = new Audio('sounds/crash.mp3'); // w
+			break;
+		case 65:
+			audio = new Audio('sounds/kick-bass.mp3'); // a
+			break;
+		case 83:
+			audio = new Audio('sounds/snare.mp3'); // s
+			break;
+		case 68:
+			audio = new Audio('sounds/tom-1.mp3'); // d
+			break;
+		case 74:
+			audio = new Audio('sounds/tom-2.mp3'); // j
+			break;
+		case 75:
+			audio = new Audio('sounds/tom-3.mp3'); // k
+			break;
+		case 76:
+			audio = new Audio('sounds/tom-4.mp3'); // l
+			break;
+		default:
+			// ...
+	}
+	
 	audio.play();
+}
+
+function flashButton(currentKey) {
+	var activeButton = document.querySelector("." + currentKey);
+	
+	activeButton.classList.add("pressed");
+	
+	setTimeout(function() {
+		activeButton.classList.remove("pressed");
+	}, 100);
 }
