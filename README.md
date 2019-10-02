87c9a859c1539746b4186d29612d6ceaf5a95528
Artur Daylidonis <artur@daylidonis.com>
2019-10-02 15:35:36 +1000

    Update Readme and Simon game

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Node JS Apps
 This is a collection of apps that use Node Javascript. Many of these apps will be simple games.
+
+## Dice
+Dice is a game for deciding who's right in an argument. It generates random die values and displays it to users, as well as determining which user won a particular round. Please do not use it to gamble.
+The game is optimised for use on mobile devices.
+
+## DrumKit
+A music app that takes user inputs from both clicks and keyboard entries.
+The game is not optimised for use on mobile devices.
+
+## Simon
+A basic implementation of the classic toy, Simon trains your attention and memory by encouraging you to memorise randomly generated colourful buttons.
+This is a work in progress.
--- simon/js/script.js
+++ simon/js/script.js
@@ -1,30 +1,41 @@
 // JavaScript Document
 let buttonColours = ["red", "blue", "yellow", "green"];
-	let gamePattern = [];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
 
 $(document).ready( function() {
 	
-  
+	// Detect keyboard presses
 	$(document).keypress( function() {
-		$(".teaser").text("Good luck!");
+		if (level == 0) {
+			$(".teaser").text("Good luck!");
+		} else {
+			$(".teaser").text("Level " + level);
+		}
+		
 		nextSequence();
 	});
 	
+	// Detect clicks
 	$(".dice-table").click( function() {
 		$(".teaser").text("Let's play!");
-		nextSequence();
+		userClickedPattern.push(this.id);
+		animateButtonClick(this.id);
 	});
 	
 });
 
 function nextSequence() {
-		let randomNumber = generateRandomNumber(4);
-		let nextColour = buttonColours[randomNumber];
+	let randomNumber = generateRandomNumber(4);
+	let nextColour = buttonColours[randomNumber];
 		
-		gamePattern.push(nextColour);
+	gamePattern.push(nextColour);
 		
-		animateButtonClick(nextColour);
-	}
+	animateButtonClick(nextColour);
+	
+	level++
+}
 
 function generateRandomNumber(max) {
 	return Math.floor(Math.random() * Math.floor(max));
@@ -33,6 +44,13 @@ function generateRandomNumber(max) {
 function animateButtonClick(id) {
 	$("#" + id).addClass("pressed");
 	setTimeout(function() {
-			$("#" + id).removeClass("pressed");
+		$("#" + id).removeClass("pressed");
 	}, 300);
+}
+
+function playGame() {
+	for (i = 0; i < buttonColours.length; i++) {
+		animateButtonClick(buttonColours[i]);
+		
+	}
 }
